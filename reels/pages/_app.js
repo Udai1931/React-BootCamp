9a62cb62446f3bc2a5c732e3e5b71002ac418eb1
Udai Gupta <udaigupta19311@gmail.com>
2022-02-16 13:41:31 +0530

    a

--- reels/pages/_app.js
+++ reels/pages/_app.js
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import './signup.css'
 import './login.css'
 import "react-responsive-carousel/lib/styles/carousel.min.css"; 
-
+import '../components/Feed.css'
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
